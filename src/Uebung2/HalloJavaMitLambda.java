e4ad81a17a4c20795ef5309c25a8a539ed1270d8
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2019-09-25 13:49:18 +0200

    normal methods without lambda

---
+++ src/Uebung2/HalloJavaMitLambda.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uebung2;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author matth
+ */
+public class HalloJavaMitLambda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Test", "Hallo", "tEsT");
+
+       for(String s : list){
+           System.out.println(s + ", ");
+       }
+       
+       
+    }
+    
+    
+}
