a0584926f1834f8e2a7bbbe8d200e1f5f6a1db5b
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2019-09-25 13:57:03 +0200

    NumberTester wrote Interface and empty Class

---
+++ src/Uebung3/NumberTester.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uebung3;
+
+/**
+ *
+ * @author matth
+ */
+public class NumberTester {
+    public NumberTester(String fileName){}
+    
+    public void setOddEvenTester(NumberTest oddTester){}
+    
+    public void setPrimeTester(NumberTest primeTreser){}
+    
+    public void setPalindromeTester(NumberTest primeTreser){}
+    
+    public void testFile(){}
+    
+}
